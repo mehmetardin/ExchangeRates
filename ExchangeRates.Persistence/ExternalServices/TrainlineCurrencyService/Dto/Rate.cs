75f0489dd9a79a99bb92d4fc308b8483aed94257
Mehmet Ardin <mehmet.ardin@paycore.com>
2021-12-07 22:56:11 +0300

    unnecesarry class remove

--- ExchangeRates.Persistence/ExternalServices/TrainlineCurrencyService/Dto/Rate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExchangeRates.Persistence.ExternalServices.TrainlineCurrencyService.Dto
-{
-    internal class Rate
-    {
-        public decimal GBP { get; set; }
-        public decimal EUR { get; set; }
-        public decimal USD { get; set; }
-    }
-}
